cfe2e2c5dcc8552f62ec9c253ba11f69194dab7d
wangfangchen <itjavawfc@163.com>
2021-12-28 16:31:52 +0800

    mybatis 增删改查 测试验证

--- src/main/java/com/deling/mybatis/mapper/dao/EmployeeMapper.java
+++ src/main/java/com/deling/mybatis/mapper/dao/EmployeeMapper.java
@@ -4,6 +4,9 @@ import com.deling.mybatis.hello.bean.Employee;
 
 public interface EmployeeMapper {
 
-      Employee getEmpById(int id);
+       Employee getEmpById(int id);
+       void addEmp(Employee employee);
+       void updateEmp(Employee employee);
+       void deleteEmpById(Integer id);
 
 }
